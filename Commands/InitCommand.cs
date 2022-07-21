f693b232a37d9b25faee500d57aec0efcd6f206f
filmee24 <filmee24@gmail.com>
2022-07-21 22:05:31 +0200

    Add Install VsCode Extension Command

--- Commands/InitCommand.cs
+++ Commands/InitCommand.cs
@@ -7,9 +7,16 @@ namespace Dotnet_Tool.Commands;
 [Verb("init", HelpText = "Init first installation of sdk/templates")]
 public class InitCommand : ICommand
 {
+    [Option("with-extension")]
+    public bool InstallExtension { get; set; }
     public void Execute()
     {
         new TemplatesCommand().Execute();
         new UpdateSdkCommand().Execute();
+
+        if(InstallExtension)
+        {
+            new InstallVsCodeExtensionCommand().Execute();
+        }
     }
 }
---
+++ Commands/InstallVsCodeExtensionCommand.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+using Dotnet_Tool.Core;
+
+namespace Dotnet_Tool.Commands;
+
+[Verb("vscode", HelpText = "Install VS Code Extension")]
+public class InstallVsCodeExtensionCommand : ICommand
+{
+    public void Execute()
+    {
+        
+    }
+}
