9340a1562a1d98e65b99f02ee9b1215717992be8
Chris <filmee24@gmail.com>
2022-09-22 18:46:58 +0200

    Add InstallAll Command

---
+++ Commands/InstallAllCommand.cs
@@ -0,0 +1,16 @@
+﻿using BacklangManager.Commands.SDK;
+using BacklangManager.Commands.Templates;
+using BacklangManager.Commands.VSCode;
+using BacklangManager.Core.TUI;
+
+namespace BacklangManager.Commands;
+
+public class InstallAllCommand : IMenuCommand
+{
+    public void Invoke(Menu parentMenu)
+    {
+        new InstallSdkCommand().Invoke(parentMenu);
+        new InstallTemplatesCommand().Invoke(parentMenu);
+        new InstallExtensionCommand().Invoke(parentMenu);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ public class Program
     public static void Main(string[] args)
     {
         var mainMenu = new Menu(null);
+        mainMenu.Items.Add("Install/Update All", new InstallAllCommand());
         mainMenu.Items.Add("Manage SDK", new ManageSdkCommand());
         mainMenu.Items.Add("Manage Templates", new ManageTemplatesCommand());
         mainMenu.Items.Add("Manage Plugins", new ManagePluginsCommand());
