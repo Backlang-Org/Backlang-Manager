5dd230a55010e73e9512eb2514d57ff7da722cb3
Chris <filmee24@gmail.com>
2022-09-25 09:32:23 +0200

    Add UpdatePluginsCommand

--- Commands/ManagePluginsCommand.cs
+++ Commands/ManagePluginsCommand.cs
@@ -8,7 +8,7 @@ public class ManagePluginsCommand : IMenuCommand
     public void Invoke(Menu parentMenu)
     {
         var menu = new Menu(parentMenu);
-        menu.Items.Add("Update", null);
+        menu.Items.Add("Update", new UpdatePluginsCommand());
         //menu.Items.Add("Search", null);
         menu.Items.Add("List Installed Plugins", new ListInstalledPluginsCommand());
         menu.Items.Add("Install Plugin", new ListAvailablePluginsCommand());
---
+++ Commands/Plugins/UpdatePluginsCommand.cs
@@ -0,0 +1,40 @@
+﻿using BacklangManager.Core;
+using BacklangManager.Core.TUI;
+using Spectre.Console;
+
+namespace BacklangManager.Commands.Plugins;
+
+public class UpdatePluginsCommand : IMenuCommand
+{
+    public void Invoke(Menu parentMenu)
+    {
+        var pluginsDir = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "Backlang", "Plugins");
+
+        if (Directory.Exists(pluginsDir))
+        {
+            AnsiConsole.Progress().Start(_ => {
+                var files = Directory.GetFiles(pluginsDir, "*.dll");
+
+                var task = _.AddTask($"Updating Plugins", maxValue: files.Count());
+
+                for (var i = 0; i < files.Length; i++)
+                {
+                    var file = files[i];
+                    var packageName = Path.GetFileNameWithoutExtension(file);
+
+                    PluginInstaller.Install(packageName);
+
+                    task.Increment(i);
+                }
+            });
+        }
+        else
+        {
+            Console.WriteLine("No Plugins Installed");
+        }
+
+        parentMenu.Show();
+    }
+}
